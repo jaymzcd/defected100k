259c980c661d27610c68b27517692c227e6281e4
jaymz <jaymz@u-dox.com>
2013-11-12 12:24:25 +0000

    add lookup and js modals for dropping image onto map; refs #21531

---
+++ map/lookup.php
@@ -0,0 +1,9 @@
+<?php
+include('config.inc.php');
+header('Content-type: application/json');
+$db = new PDO("mysql:host=localhost;dbname=def100k", $user, $passwd);
+$stmt = $db->prepare('SELECT * FROM images WHERE username=?');
+$stmt->bindParam(1, $_GET['handle']);
+$stmt->execute();
+$data = $stmt->fetch(PDO::FETCH_ASSOC);
+echo json_encode($data);
